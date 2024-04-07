39c74e9bf2ea411e6b4f7bdf3e83dcdd735b9245
nazmul_hossin <mdnazmulhossin999@gmail.com>
2024-04-07 12:12:16 +0600

    working on theme

--- js/script.js
+++ js/script.js
@@ -51,6 +51,32 @@ main.onscroll = () => {
     });
 }
 
+// changing theme and color
+// show window on click
+document.getElementById('theme-changing-button').addEventListener("click", () => {
+    document.getElementById('theme').classList.toggle("show-them-window");
+});
+
+// changing theme on load
+const currentTheme = localStorage.getItem('lightTheme');
+const mainContainer = document.querySelector('.main-container');
+window.onload = function () {
+    if(currentTheme == 'enabled')
+        mainContainer.classList.add("light-theme");
+};
+
+// changing theme on click
+document.getElementById('light_theme').addEventListener("click", () => {
+    mainContainer.classList.add("light-theme");
+    localStorage.setItem('lightTheme', 'enabled');
+});
+
+document.getElementById('dark_theme').addEventListener("click", () => {
+    mainContainer.classList.remove("light-theme");
+    localStorage.setItem('lightTheme', '');
+});
+
+
 // Home page background animation 
 particlesJS("particles-js", {"particles":{"number":{"value":80,"density":{"enable":true,"value_area":800}},"color":{"value":"#ffffff"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.5,"random":false,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":2,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":true,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":6,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"repulse"},"onclick":{"enable":true,"mode":"push"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});
 
