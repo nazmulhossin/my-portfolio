d0d6a3ba9077b7be96e2cad814e30895369b5a7e
nazmul_hossin <mdnazmulhossin999@gmail.com>
2024-04-08 19:06:35 +0600

    changing theme feature completed

--- js/script.js
+++ js/script.js
@@ -76,6 +76,54 @@ document.getElementById('dark_theme').addEventListener("click", () => {
     localStorage.setItem('lightTheme', '');
 });
 
+// changing color on click
+document.querySelector('#color-switcher div').addEventListener('click', function(event) {
+    if(event.target.parentNode.classList.contains('color')) {
+        const colorName = event.target.className;
+        document.querySelector(".color-applyed").classList.remove("color-applyed");
+        event.target.parentNode.classList.add("color-applyed");
+
+        if(colorName == "caribbean-green") {
+            document.documentElement.style.setProperty('--link-color', "#069c7a");
+            document.documentElement.style.setProperty('--link-hover-color', "#00BC91");
+        }
+
+        else if(colorName == "yellow") {
+            document.documentElement.style.setProperty('--link-color', "#e3aa00");
+            document.documentElement.style.setProperty('--link-hover-color', "#FFC107");
+        }
+
+        else if(colorName == "orange") {
+            document.documentElement.style.setProperty('--link-color', "#fa5908");
+            document.documentElement.style.setProperty('--link-hover-color', "#FF7004");
+        }
+
+        else if(colorName == "punch") {
+            document.documentElement.style.setProperty('--link-color', "#CD2636");
+            document.documentElement.style.setProperty('--link-hover-color', "#dc3545");
+        }
+        
+        else if(colorName == "purple") {
+            document.documentElement.style.setProperty('--link-color', "#5F37A8");
+            document.documentElement.style.setProperty('--link-hover-color', "#6f42c1");
+        }
+
+        else if(colorName == "green") {
+            document.documentElement.style.setProperty('--link-color', "#218A39");
+            document.documentElement.style.setProperty('--link-hover-color', "#28a745");
+        }
+
+        else if(colorName == "violet") {
+            document.documentElement.style.setProperty('--link-color', "#763385");
+            document.documentElement.style.setProperty('--link-hover-color', "#803790");
+        }
+
+        else if(colorName == "cyan") {
+            document.documentElement.style.setProperty('--link-color', "#0BADCE");
+            document.documentElement.style.setProperty('--link-hover-color', "#0dcaf0");
+        }
+    }
+});
 
 // Home page background animation 
 particlesJS("particles-js", {"particles":{"number":{"value":80,"density":{"enable":true,"value_area":800}},"color":{"value":"#ffffff"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.5,"random":false,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":2,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":true,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":6,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"repulse"},"onclick":{"enable":true,"mode":"push"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});
